00c94049cc763fc18edad4b8dd57d1cdc8ea3d1b
Bharattt-Jayalwal <bharatjayalwal@gmail.com>
2026-09-09 00:33:29 +0530

    Add files via upload

---
+++ 90 DAYS PYTHON/DAY-3.py
@@ -0,0 +1,39 @@
+#DAY 3 OF 90 DAYS
+
+'''a=12
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print(a%b)
+print(a**b)
+print(a//b)'''
+
+username=input("Enter username:")
+password=int(input("Enter password:"))
+username_correct="Bharat"
+password_correct=76
+print(username_correct=="Bharat" and password_correct==76)
+
+
+'''salary=30000
+salary+=5000
+salary-=2000
+print(salary)
+
+balance=5000
+balance+=1000
+balance-=750
+balance+=500
+balance-=1250
+print(balance)
+
+a=12
+b=6
+print(a>b)
+print(a==b)
+print(a!=b)'''
+
+
